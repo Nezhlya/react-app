f002ecca01d665a7b75562100c5598de56e46ee0
nezhlya <eminejla@gmail.com>
2023-07-03 12:50:41 +0200

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
-import SearchEngine from './component/SearchEngine.js';
+import SearchEngine from "./component/SearchEngine.js";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-      < SearchEngine />
-      <small><a href="https://github.com/Nezhlya/react-app" target="_black" rel="noreferrer">Open-source code</a> by Nezhlya Emin</small>
+        <SearchEngine />
+        <small>
+          <a
+            href="https://github.com/Nezhlya/react-app"
+            target="_black"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Nezhlya Emin
+        </small>
       </header>
     </div>
   );
